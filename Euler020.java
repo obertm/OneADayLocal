bb1145146424361d720d87f467dbae60e8e004ef
Obert Merchant <112640974+obertm@users.noreply.github.com>
2025-08-17 14:08:51 +0000

    feat(euler020): factorial digit sum for 100! -> 648

--- Euler020.java
+++ Euler020.java
@@ -1,7 +1,21 @@
 // Euler 020: Factorial digit sum
-// TODO: Implement solution
+import java.math.BigInteger;
+
 public final class Euler020 {
+    public static int sumDigitsOfFactorial(int n) {
+        BigInteger fact = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) fact = fact.multiply(BigInteger.valueOf(i));
+        String s = fact.toString();
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) sum += s.charAt(i) - '0';
+        return sum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("TODO");
+        int n = 100;
+        if (args != null && args.length > 0) {
+            try { n = Integer.parseInt(args[0]); } catch (Exception ignored) {}
+        }
+        System.out.println(sumDigitsOfFactorial(n));
     }
 }
