68714da20b990e79caff105074674e6bbf41eaee
Obert Merchant <112640974+obertm@users.noreply.github.com>
2025-08-17 12:30:00 +0000

    feat(euler016): sum of digits of 2^1000 (1366) using BigInteger

--- Euler016.java
+++ Euler016.java
@@ -1,7 +1,20 @@
 // Euler 016: Power digit sum
-// TODO: Implement solution
+import java.math.BigInteger;
+
 public final class Euler016 {
+    public static int sumDigitsOfTwoPow(int n) {
+        BigInteger val = BigInteger.ONE.shiftLeft(n); // 2^n
+        String s = val.toString();
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) sum += s.charAt(i) - '0';
+        return sum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("TODO");
+        int n = 1000;
+        if (args != null && args.length > 0) {
+            try { n = Integer.parseInt(args[0]); } catch (Exception ignored) {}
+        }
+        System.out.println(sumDigitsOfTwoPow(n));
     }
 }
