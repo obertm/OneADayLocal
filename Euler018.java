0ce8c35ebd4c8acb39f33d22257caf00affa612a
Obert Merchant <112640974+obertm@users.noreply.github.com>
2025-08-17 13:45:59 +0000

    feat(euler018): maximum path sum I via bottom-up DP (1074)

--- Euler018.java
+++ Euler018.java
@@ -1,7 +1,37 @@
 // Euler 018: Maximum path sum I
-// TODO: Implement solution
 public final class Euler018 {
+    private static final int[][] TRIANGLE = new int[][]{
+        {75},
+        {95, 64},
+        {17, 47, 82},
+        {18, 35, 87, 10},
+    {20, 4, 82, 47, 65},
+    {19, 1, 23, 75, 3, 34},
+    {88, 2, 77, 73, 7, 63, 67},
+    {99, 65, 4, 28, 6, 16, 70, 92},
+        {41, 41, 26, 56, 83, 40, 80, 70, 33},
+        {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+        {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+        {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+        {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+    {63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+    {4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23}
+    };
+
+    public static int maxPathSum(int[][] tri) {
+        int n = tri.length;
+        int[][] dp = new int[n][];
+        dp[n - 1] = tri[n - 1].clone();
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = new int[tri[i].length];
+            for (int j = 0; j < tri[i].length; j++) {
+                dp[i][j] = tri[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
-        System.out.println("TODO");
+        System.out.println(maxPathSum(TRIANGLE));
     }
 }
